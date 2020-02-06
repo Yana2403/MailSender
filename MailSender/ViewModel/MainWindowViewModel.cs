d19f0a04fcbf0acd30aea96273ab29a955747326
Yana2403 <ulendeeva.yana2013@yandex.ru>
2020-02-06 19:41:30 +0400

    Добавление первых провайдеров данных и мест их хранения

--- MailSender/ViewModel/MainWindowViewModel.cs
+++ MailSender/ViewModel/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 using GalaSoft.MvvmLight;
+using MailSender.lib.Entities;
 using MailSender.lib.Services;
+using System.Collections.ObjectModel;
 
 namespace MailSender.ViewModel
 {
@@ -13,9 +15,18 @@ namespace MailSender.ViewModel
             get => _Title;
             set => Set(ref _Title, value);
         }
+        private ObservableCollection<Adressee> _Adressees; //определяем коллекцию
+        public ObservableCollection<Adressee> Adressees //для возможности обновления
+        {
+            get => _Adressees;
+            private set => Set(ref _Adressees, value);
+
+        }
+
         public MainWindowViewModel(AdresseeManager AdresseeManager)
         {
             _AdresseeManager = AdresseeManager;
+            _Adressees = new ObservableCollection<Adressee>(_AdresseeManager.GetAll());
         }
     }
 }
