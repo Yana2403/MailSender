0bbcbdd5d4a93678279037d65a1cfc8801105cbc
Yana2403 <ulendeeva.yana2013@yandex.ru>
2020-02-10 23:31:52 +0400

    Второй тест

---
+++ MailSender/Tests/MailSender.lib.Tests/Services/AdresseeStoreInMemoryTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MailSender.lib.Services.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSender.lib.Tests.Services
+{
+   public class AdresseeStoreInMemoryTest
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentNullException))] //указание ожидаемого исключения - 1 вариант
+        public void Create_throw_ArgumentNullException_if_items_is_null() 
+        {
+            var store = new AdresseeStoreinMemory();
+
+            store.Create(null);
+        }
+
+        [TestMethod] //2 вариант - без указания исключения
+        public void Create_throw_ArgumentNullException_if_items_is_null2() 
+        {
+            var store = new AdresseeStoreinMemory();
+
+            Assert.ThrowsException<ArgumentNullException>(() => store.Create(null));
+        }
+    }
+}
