8fe1c74dfc974c8cf2444d3bd408d67cb87779b9
Yana2403 <ulendeeva.yana2013@yandex.ru>
2020-02-10 22:57:39 +0400

    Первый модульный тест

---
+++ MailSender/Tests/MailSender.lib.Tests/Service/TestEncoderTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailSender.lib.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSender.lib.Tests.Service
+{
+    [TestClass]
+    public class TestEncoderTests //должен быть публичный и не абстрактный
+    {
+        [TestMethod]
+        public void Encode_ABC_to_BCD_with_key_1() //описание сценария тесты
+        {
+            //A-A-A = Arange(Подготовка данных) -Act(получаем актуальный результат)- Assert -3 этапа
+            const string str = "ABC"; //то, что есть
+            const int key = 1;
+            const string expected_str = "BCD"; //ожидаемое
+            var actual_str = TextEncoder.Encode(str, key);
+
+            Assert.AreEqual(expected_str, actual_str); //утверждаем, что значение 1 = значению 2
+        }
+    }
+}
