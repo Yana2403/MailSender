5d88f45634839e738c32c5e14ace0005e09b17fa
Yana2403 <ulendeeva.yana2013@yandex.ru>
2020-02-24 16:40:47 +0400

    ассинхронная рассылка почты

--- MailSender.lib/Services/MailSender.cs
+++ MailSender.lib/Services/MailSender.cs
@@ -3,7 +3,9 @@ using System.Diagnostics;
 using System.Net;
 using System.Net.Mail;
 using System.Threading;
+using System.Threading.Tasks;
 using MailSender.lib.Entities;
+using System.Linq;
 
 namespace MailSender.lib.Services
 {
@@ -38,6 +40,33 @@ namespace MailSender.lib.Services
         {
             foreach (var adressee in To)
                 ThreadPool.QueueUserWorkItem(_ => Send(Message, From, adressee));
+        }
+        public async Task SendAsync(Mail Mail, Adresser From, Adressee To) //отправка одного сообщения асинхронно
+        {
+            using (var message = new MailMessage(new MailAddress(From.Address, From.Name), new MailAddress(To.Address, To.Name)))
+            {
+                message.Subject = Mail.Subject;
+                message.Body = Mail.Body;
+
+                var login = new NetworkCredential(_Server.Login, _Server.Password);
+                using (var client = new SmtpClient(_Server.Address, _Server.Port) { EnableSsl = _Server.UseSSL, Credentials = login })
+                    //client.Send(message);
+                    await client.SendMailAsync(message).ConfigureAwait(false);
+            }
+        }
+
+        /*public async Task SendAsync(Mail Message, Adresser From, IEnumerable<Adressee> To) //отправка сразу нескольким параллельной обработкой
+        {
+            await Task.WhenAll(To.Select(to => SendAsync(Message, From, to))).ConfigureAwait(false);
+        }*/
+
+        public async Task SendAsync(Mail Message, Adresser From, IEnumerable<Adressee> To, CancellationToken Cancel = default) //2 вариат рассылки последовательной обработкой
+        {
+            foreach (var recipient in To)
+            {
+                Cancel.ThrowIfCancellationRequested();
+                await SendAsync(Message, From, recipient).ConfigureAwait(false);
+            }
         }
     }
 }
