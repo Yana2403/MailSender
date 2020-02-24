5940fc937ae71215218215df5a0c04e50a29544e
Yana2403 <ulendeeva.yana2013@yandex.ru>
2020-02-24 21:05:35 +0400

    Контекст базы данных

---
+++ MailSender.lib/Data/EF/MailSenderDB.cs
@@ -0,0 +1,27 @@
+﻿using MailSender.lib.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.lib.Data.EF
+{
+    public class MailSenderDB: DbContext
+    {
+        public DbSet<Mail> Mails { get; set; }
+
+        public DbSet<Adresser> Senders { get; set; }
+
+        public DbSet<Adressee> Recipients { get; set; }
+
+        public DbSet<Server> Servers { get; set; }
+
+        public DbSet<MailingList> MailingLists { get; set; }
+
+        public DbSet<SchedulerTask> SchedulerTasks { get; set; }
+
+        public MailSenderDB(DbContextOptions<MailSenderDB> opt) : base(opt) { }
+    }
+}
