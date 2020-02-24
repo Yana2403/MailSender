8d1eaf09e0b86959a9df8577d6bbeb5389f63b37
Yana2403 <ulendeeva.yana2013@yandex.ru>
2020-02-24 20:52:58 +0400

    конфигурация приложения

--- MailSender/App.xaml.cs
+++ MailSender/App.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 
 namespace MailSender
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App 
     {
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder() //интеграция свойств
+           .SetBasePath(Environment.CurrentDirectory)
+           .AddJsonFile("appsettings.json")
+           .Build();
     }
 }
